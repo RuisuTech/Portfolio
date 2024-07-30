8a48fbe55e7f11ca02d26972960bf4afc3c5e920
Ruisu <JoseLuisOsorioGuzman11@gmail.com>
2024-07-30 14:54:56 -0500

    Nav

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,7 @@ import { Link } from 'react-router-dom';
 
 function Header() {
   return (
-    <header>
+    <header className='sticky top-0 z-40 w-full bg-[#171717E6] backdrop-blur-sm'>
       <div className="flex md:justify-between justify-center items-center p-4">
         <h1 className="text-4xl font-bold hidden md:block">Logo</h1>
         <div className="flex flex-wrap justify-between items-center gap-4 text-lg font-normal pt-2">
