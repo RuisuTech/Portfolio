33fb63b5c9c4c8ace0449658b555b3e0d531e474
Ruisu <JoseLuisOsorioGuzman11@gmail.com>
2024-07-30 16:12:32 -0500

    Error 404

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    outDir: 'dist',  // Directorio de salida para la construcción
+  },
+  server: {
+    // Opcional: configuración del servidor de desarrollo
+  }
+});
