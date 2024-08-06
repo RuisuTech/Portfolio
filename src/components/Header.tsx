0c7d7733a15c576b38c2ae53c6ef50081c66e18c
Ruisu <JoseLuisOsorioGuzman11@gmail.com>
2024-08-05 19:52:16 -0500

    Casi un logo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,7 +4,7 @@ function Header() {
   return (
     <header className='sticky top-0 z-40 w-full bg-[#171717E6] backdrop-blur-sm'>
       <div className="flex md:justify-between justify-center items-center p-4">
-        <h1 className="text-4xl font-bold hidden md:block">Logo</h1>
+        <Link className="text-4xl font-bold hidden md:block" to="/">JLG</Link>
         <div className="flex flex-wrap justify-between items-center gap-4 text-lg font-normal pt-2">
           <Link to="/">Sobre mi</Link>
           <Link to="/Proyectos">Proyectos</Link>
