1688bb178a98d31b4c5955633acf08c72090d38e
Ruisu <JoseLuisOsorioGuzman11@gmail.com>
2024-08-06 12:44:08 -0500

    Boton

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 
 function Header() {
@@ -8,24 +8,44 @@ function Header() {
     setIsOpen(!isOpen);
   };
 
+  useEffect(() => {
+    if (isOpen) {
+      document.body.style.overflow = "hidden";
+    } else {
+      document.body.style.overflow = "auto";
+    }
+    return () => {
+      document.body.style.overflow = "auto";
+    };
+  }, [isOpen]);
+
   return (
     <header className="fixed top-0 z-40 px-[10%] w-screen h-[72px] bg-[#171717E6] backdrop-blur-lg">
       <div className="flex justify-between h-full items-center gap-4">
-        <Link
-          className="text-4xl font-bold hover:text-[#1d9c53]"
-          to="/"
-        >
+        <Link className="text-4xl font-bold hover:text-[#1d9c53]" to="/">
           JLG
         </Link>
 
         {/* Botón de hamburguesa */}
         <button
-          className="md:hidden flex flex-col justify-center items-center w-8 h-8"
+          className="md:hidden flex flex-col justify-center items-center w-8 h-8 relative"
           onClick={toggleMenu}
         >
-          <span className="block w-full h-[2px] bg-white my-1"></span>
-          <span className="block w-full h-[2px] bg-white my-1"></span>
-          <span className="block w-full h-[2px] bg-white my-1"></span>
+          <span
+            className={`block w-full h-[2px] bg-white absolute transition-transform duration-300 ${
+              isOpen ? "rotate-45" : "mb-5"
+            }`}
+          ></span>
+          <span
+            className={`block w-full h-[2px] bg-white absolute transition-opacity duration-300 ${
+              isOpen ? "opacity-0" : ""
+            }`}
+          ></span>
+          <span
+            className={`block w-full h-[2px] bg-white absolute transition-transform duration-300 ${
+              isOpen ? "-rotate-45" : "mt-5"
+            }`}
+          ></span>
         </button>
 
         {/* Menú de navegación */}
