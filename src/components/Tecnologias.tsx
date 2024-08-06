ce44d49ee5534b0450188bec30f2fbeec5c1ae00
Ruisu <JoseLuisOsorioGuzman11@gmail.com>
2024-08-05 19:34:54 -0500

    Img Tecnologias

--- src/components/Tecnologias.tsx
+++ src/components/Tecnologias.tsx
@@ -8,11 +8,11 @@ function Tecnologias() {
             <div className="flex flex-col justify-center items-center">
               <h1>Front-End</h1>
               <div className="flex flex-wrap justify-center gap-4">
-                <img className="object-cover w-[75px] h-[75px] mt-4" src="Html5.webp" alt="Html5" />
-                <img className="object-cover w-[75px] h-[75px] mt-4" src="CSS.webp" alt="CSS" />
-                <img className="object-cover w-[75px] h-[75px] mt-4" src="JS.webp" alt="JS" />
-                <img className="object-cover w-[75px] h-[75px] mt-4" src="TS.webp" alt="TS" />
-                <img className="object-cover w-[75px] h-[75px] mt-4" src="React.webp" alt="React" />
+                <img className="w-auto h-[75px] mt-4" src="Html5.webp" alt="Html5" />
+                <img className="w-auto h-[75px] mt-4" src="CSS.webp" alt="CSS" />
+                <img className="w-auto h-[75px] mt-4" src="JS.webp" alt="JS" />
+                <img className="w-auto h-[75px] mt-4" src="TS.webp" alt="TS" />
+                <img className="w-auto h-[75px] mt-4" src="React.webp" alt="React" />
               </div>
             </div>
           </div>
@@ -22,9 +22,9 @@ function Tecnologias() {
             <div className="flex flex-col justify-center items-center">
               <h1>Back-End</h1>
               <div className="flex flex-wrap justify-center gap-4">
-                <img className="object-cover w-[75px] h-[75px] mt-4" src="Java.webp" alt="Java" />
-                <img className="object-cover w-[75px] h-[75px] mt-4" src="Spring.webp" alt="Spring" />
-                <img className="object-cover w-[75px] h-[75px] mt-4" src="SQL.webp" alt="SQL" />
+                <img className="w-auto h-[75px] mt-4" src="Java.webp" alt="Java" />
+                <img className="w-auto h-[75px] mt-4" src="Spring.webp" alt="Spring" />
+                <img className="w-auto h-[75px] mt-4" src="SQL.webp" alt="SQL" />
               </div>
             </div>
           </div>
@@ -34,8 +34,8 @@ function Tecnologias() {
             <div className="flex flex-col justify-center items-center">
               <h1>Development tools</h1>
               <div className="flex flex-wrap justify-center gap-4">
-                <img className="object-cover w-[75px] h-[75px] mt-4" src="Git.webp" alt="GIT" />
-                <img className="object-cover w-[75px] h-[75px] mt-4" src="GitHub.webp" alt="GitHub" />
+                <img className="w-auto h-[75px] mt-4" src="Git.webp" alt="GIT" />
+                <img className="w-auto h-[75px] mt-4" src="GitHub.webp" alt="GitHub" />
               </div>
             </div>
           </div>
